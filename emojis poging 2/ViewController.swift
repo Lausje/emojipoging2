53ae3f776684747517abfa99289865fbf9fcd44b
Laura van der Stee <lauravanderstee@gmail.com>
2017-04-02 16:16:21 +0200

    Werkende tabel en 2e scherm toegevoegd

--- emojis poging 2/ViewController.swift
+++ emojis poging 2/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var mijnTabelOutlet: UITableView!
+    // de outlet die een connectie met de tabel in de storyboard geeft
 
+    var beestenbende = ["😸","🐹","🐝","🐧","🦋","🐳","🐘","🐠"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mijnTabelOutlet.dataSource = self
+        mijnTabelOutlet.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       // beantwoordt de vraag hoeveel regels er in de tabel moeten zijn
+        return beestenbende.count
+        // antwoord: hoeveel karakters er in de array "beestenbende" zitten
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // bepaalt wat er in elke cel moet
+        let cell = UITableViewCell()
+        // definieert cell als een UITableViewCell, want dat is wat de functie als output verwacht
+        cell.textLabel?.text = beestenbende[indexPath.row]
+        //bepaalt een tekstlabel voor in elke cel. bepaalt op basis van de index hoeveelste karakter van de array wordt getoond
+        return cell
+        // geeft de verwachte output van de functie
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
