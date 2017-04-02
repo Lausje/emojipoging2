9f31d54ead662c7de4eb12b3c639071fa5649af8
Laura van der Stee <lauravanderstee@gmail.com>
2017-04-02 15:17:42 +0200

    Initial Commit

---
+++ emojis poging 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emojis poging 2
+//
+//  Created by Laura van der Stee on 02-04-17.
+//  Copyright © 2017 Laura van der Stee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
